7b06c3877a140b4ffdc1b1c0a25f16ff169838eb
Charalampos Livas <livas.charalampos@gmail.com>
2026-03-02 13:56:50 -0500

    Update CIF example to use kcal/mol for energy

--- examples/example_single_cif.py
+++ examples/example_single_cif.py
@@ -17,10 +17,11 @@ try:
     if success:
         print(f"Convergence Status: {mof.opt_status}")
         print(f"Linker SMILES:      {mof.linker_smiles}")
-        print(f"Delta E (DE):       {mof.de:.4f} eV")
+        print(f"Delta E (DE):       {mof.de:.4f} kcal/mol")
         print(f"RMSD:               {mof.rmsd:.4f} Å")
     else:
         print(f"Evaluation failed. Status: {mof.opt_status}")
 
 except Exception as e:
     print(f"An error occurred: {e}")
+
